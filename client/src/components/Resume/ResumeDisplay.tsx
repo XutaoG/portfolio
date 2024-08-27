2f4ab03de0fe7c9348e24064b2d4837d139f234f
Xutao Gao <taogeeee@gmail.com>
2024-08-26 21:52:52 -0400

    Actually fix resume viewing in deployment build

--- client/src/components/Resume/ResumeDisplay.tsx
+++ client/src/components/Resume/ResumeDisplay.tsx
@@ -1,9 +1,11 @@
+import resume from "../../assets/resume/xutao-gao-resume.pdf";
+
 const ResumeDisplay = () => {
 	return (
 		<div className="w-full flex flex-col gap-2">
 			<embed
 				className="w-full aspect-[3/4]"
-				src="/src/assets/resume/xutao-gao-resume.pdf#view=FitH"
+				src={`${resume}#view=FitH`}
 				type="application/pdf"
 			/>
 			<p className="inter tracking-wider self-end">Last Updated: 8/24</p>
